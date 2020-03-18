bc9d2278bd98fcc246fe321e3b6cd6ffa4bb25d9
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-18 00:24:47 +0000

    import components and test 'go to onClick'

--- sistecaut/src/pages/Index.vue
+++ sistecaut/src/pages/Index.vue
@@ -1,27 +1,50 @@
 <template>
   <Layout>
-    <Slicer />
-    <Spotlight />  
+    <CarouselWrapper />
+    <Spotlight />
+    <Footer />  
+    <About />
+    <Contacts />
+
+<div class="main" id="section1">
+  <h2>Section 1</h2>
+  <p>Click on the link to see the "smooth" scrolling effect.</p>
+  <a href="#section2">Click Me to Smooth Scroll to Section 2 Below</a>
+  <p>Note: Remove the scroll-behavior property to remove smooth scrolling.</p>
+</div>
+
+<div class="main" id="section2">
+  <h2>Section 2</h2>
+  <a href="#section1">Click Me to Smooth Scroll to Section 1 Above</a>
+</div>
+
   </Layout>
 </template>
 
 <script>
-import NavBar from '../components/NavBar'
-import Slicer from '../components/Slider'
-import Spotlight from '../components/Spotlight'
-
+import NavBar from '@/components/TheNavbar'
+import CarouselWrapper from '@/components/carousel/CarouselWrapper'
+import Spotlight from '@/components/TheSpotlight'
+import Footer from '@/components/TheFooter'
+import About from '@/components/About'
+import Contacts from '@/components/TheContacts'
 export default {
-  components: {
+  components: { // insert here the name of the component
     NavBar,
-    Slicer,
-    Spotlight
+    CarouselWrapper,
+    Spotlight,
+    Footer,
+    About,
+    Contacts
   },
   name: 'Sistecaut'
 }
 </script>
 
 <style>
+
 .home-links a {
-  margin-right: 1rem;
+  margin-right: .1rem;
 }
 </style>
+
