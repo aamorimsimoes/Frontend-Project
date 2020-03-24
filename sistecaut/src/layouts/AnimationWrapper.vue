ef9aec62310d43b7962c4539a32199b1febbf2cb
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-24 22:18:42 +0000

    add animation to page slider (in progress)

---
+++ sistecaut/src/layouts/AnimationWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnimationWrapper",
+  props: ["currentPage", "direction"],
+  watch: {
+    currentPage: function(newVal, oldVal) {
+      // watch it
+      console.log("Prop changed: ", newVal, " | was: ", oldVal);
+    }
+  }
+};
+</script>
+
+<style></style>
