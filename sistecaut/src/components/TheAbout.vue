7a3355b27610c3cf4ac33f19f6fc34006e52ddbe
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-24 21:54:41 +0000

    edit <style>

--- sistecaut/src/components/TheAbout.vue
+++ sistecaut/src/components/TheAbout.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="about">
-    ABOUT US
+    <h1>ABOUT US</h1>
     <p>
-      A Sistecaut Lda foi criada em novembro de 2003, tem sede em Aveiro e está focalizada no fornecimento de máquinas especiais e soluções de automação.
+      A Sistecaut Lda foi criada em novembro de 2003, tem sede em Aveiro e está
+      focalizada no fornecimento de máquinas especiais e soluções de automação.
     </p>
     <p>
-    Apresenta-se no mercado dotada de meios humanos, técnicos e operacionais, para superar as crescentes exigências dos clientes. Consciente dos seus recursos e saber, persegue novos desafios e áreas de ação, numa procura contínua de desenvolvimento sustentado e aumento de valor na cadeia em que participa.
+      Apresenta-se no mercado dotada de meios humanos, técnicos e operacionais,
+      para superar as crescentes exigências dos clientes. Consciente dos seus
+      recursos e saber, persegue novos desafios e áreas de ação, numa procura
+      contínua de desenvolvimento sustentado e aumento de valor na cadeia em que
+      participa.
     </p>
     <p>
-    Tem capacidade para projetar e construir sistemas por medida, para processo industrial ou automatização do mesmo.
+      Tem capacidade para projetar e construir sistemas por medida, para
+      processo industrial ou automatização do mesmo.
     </p>
     <p>
-    Integra as tecnologias mais recentes, tais como robótica, servo acionamentos e visão artificial. O seu conhecimento e capacidade são reconhecidos por alguns parceiros no sector automóvel e de eletrónica, no desenvolvimento de máquinas especiais e sistemas de controlo para os seus processos de fabrico. Participa também no sector alimentar e plásticos, desenvolvendo sistemas de produção, controlo e monitorização de processos.
+      Integra as tecnologias mais recentes, tais como robótica, servo
+      acionamentos e visão artificial. O seu conhecimento e capacidade são
+      reconhecidos por alguns parceiros no sector automóvel e de eletrónica, no
+      desenvolvimento de máquinas especiais e sistemas de controlo para os seus
+      processos de fabrico. Participa também no sector alimentar e plásticos,
+      desenvolvendo sistemas de produção, controlo e monitorização de processos.
     </p>
     <p>
-    A Sistecaut, Lda tem sido distinguida pelos seus parceiros e clientes com confiança e responsabilidade, participando em contratos de elevado valor e relevância tecnológica.
+      A Sistecaut, Lda tem sido distinguida pelos seus parceiros e clientes com
+      confiança e responsabilidade, participando em contratos de elevado valor e
+      relevância tecnológica.
     </p>
   </div>
 </template>
@@ -22,20 +35,27 @@
 <script>
 export default {
   metaInfo: {
-    title: 'About Us'
+    title: "About Us"
   },
-  name: 'About'
-}
+  name: "About"
+};
 </script>
 
 <style>
-  /* The sizes definitions can also be strings, you will never know
+/* The sizes definitions can also be strings, you will never know
   every single one, but you can get hints by changing these attributes
   at the inspect of the page 
   */
+#about {
+  background-color: lightgray;
+  width: 90vw;
+  height: 95vh;
+  padding: 5vh 5vw 5vh 5vw;
+}
+
+@media (min-width: 1025px) {
   #about {
-    background-color:cadetblue;
-    width: 100%; 
-    height: fit-content;
+    height: 80vh;
   }
+}
 </style>
