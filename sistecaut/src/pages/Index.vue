06714b7a50bbbb62e5506a4211324af4c9856b4f
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-25 19:06:01 +0000

    import SVG as component to improve Arrow Up & Down

--- sistecaut/src/pages/Index.vue
+++ sistecaut/src/pages/Index.vue
@@ -1,19 +1,19 @@
 <template>
   <Layout>
-    <button
+    <!-- <button
       v-if="currentPage > 0"
       v-on:click="handlePreviousClick"
       class="slideButtons slideUp"
-    >
-      &#8963;
-    </button>
-    <button
+    > -->
+      <ArrowUp class="slideButtons slideUp" v-on:click="handlePreviousClick" v-if="currentPage > 0" />
+    <!-- </button> -->
+    <!-- <button
       v-if="currentPage < nPages - 1"
       v-on:click="handleNextClick"
       class="slideButtons slideDown"
-    >
-      &#8964;
-    </button>
+    > -->
+      <ArrowDown class="slideButtons slideDown" v-on:click="handleNextClick" v-if="currentPage < nPages - 1" />
+    <!-- </button> -->
     <AnimationWrapper currentPage="currentPage" direction="direction">
       <Carousel v-if="currentPage === 0" />
       <About v-else-if="currentPage === 1" />
@@ -46,6 +46,8 @@ import Footer from "@/components/TheFooter";
 import About from "@/components/TheAbout";
 import Layout from "@/layouts/Default";
 import AnimationWrapper from "@/layouts/AnimationWrapper";
+import ArrowUp from "@/assets/svgs/chevrons-up.svg";
+import ArrowDown from "@/assets/svgs/chevrons-down.svg"
 
 export default {
   components: {
@@ -55,7 +57,9 @@ export default {
     Footer,
     About,
     Layout,
-    AnimationWrapper
+    AnimationWrapper,
+    ArrowUp,
+    ArrowDown
   },
   name: "Sistecaut",
   data() {
@@ -79,12 +83,12 @@ export default {
       } else {
         this.currentPage = this.nPages - 1;
       }
-    },
+    }
   }
 };
 </script>
 
-<style>
+<style lang='scss'>
 .home-links a {
   margin-right: .1rem;
 }
@@ -92,11 +96,11 @@ export default {
 .slideButtons {
   z-index: 99;
   position: absolute;
-  left: 1vw;
+  left: .2vw;
   color: white;
-  background-color: rgba(255, 183, 0, .5);
-  height: 5vh;
-  width: 5vh;
+  /* background-color: rgba(255, 183, 0, .5); */
+  height: 10vh;
+  width: 10vh;
   border-radius: 10vh;
   border: none;
   font-weight: lighter;
