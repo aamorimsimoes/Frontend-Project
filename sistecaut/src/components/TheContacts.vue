5c764a16cc43df637682f87d3e1978c6915c43e5
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-25 19:04:46 +0000

    add form to use with netlify service

--- sistecaut/src/components/TheContacts.vue
+++ sistecaut/src/components/TheContacts.vue
@@ -7,6 +7,23 @@
       <li>Website: {{web}}</li>
     </ul>
   </p>
+  <div class="formContainer">
+    <h1>Contacto</h1>
+    <form name="contact" method="POST" data-netlify="true">
+      <input type="text" name="name" placeholder="Full Name">
+      <input type="email" name="email" placeholder="Email">
+      <label>
+        Your Role: 
+      <select name="role[]" multiple>
+        <option value="leader">Leader</option>
+        <option value="follower">Follower</option>
+      </select>
+    </label>
+      <input type="email" placeholder="sistecaut@example.com" disabled>
+      <textarea name="message" cols="30" rows="10" placeholder="Message"></textarea>
+      <button type="submit">Send</button>
+    </form>
+  </div>
   </div>
 </template>
 
@@ -35,5 +52,29 @@ export default {
   .contactMe {
     height: 40vh;
   }
+}
+
+form input,
+form textarea {
+  display: block;
+  width: 500px;
+  padding: 20px;
+  background-color: lightgray;
+  border: 3px solid gray;
+  margin: 20px 0;
+  outline: none;
+  font-size: 1.2rem;
+  color: black;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  transform: translateX(-50%);
+}
+
+form input:last-child {
+  width: 150px;
+  background-color: darkgray;
+  border: none;
+  cursor: pointer;
 }
 </style>
