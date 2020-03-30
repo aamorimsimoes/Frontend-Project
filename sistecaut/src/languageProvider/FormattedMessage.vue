44aea9ec354490c7ac9d0a22b671837fe1d1c13c
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-30 22:15:47 +0100

    first draft for language provider

---
+++ sistecaut/src/languageProvider/FormattedMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+    {{ getTranslation(id) }}
+  </span>
+</template>
+
+<script>
+import MessagesEN from "./EN_en.json";
+import MessagesPT from "./PT_pt.json";
+
+export default {
+  name: "LanguageProvider",
+  props: ["id"],
+  methods: {
+    getTranslation: function(id) {
+      const currentLanguage = localStorage.getItem("language") || "english";
+
+      switch (currentLanguage) {
+        case "portuguese":
+          return MessagesPT[id];
+        default:
+          return MessagesEN[id];
+      }
+    }
+  }
+};
+</script>
---
+++ sistecaut/src/languageProvider/LanguageProvider.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+// export function changeLanguage(newLanguage) {
+//   localStorage.setItem("language", newLanguage);
+//   locale = newLanguage;
+// }
+export default {
+  name: "LanguageProvider",
+  data() {
+    return {
+      locale: localStorage.getItem("language") || "english"
+    };
+  },
+  methods: {
+    changeLanguage: function(newLanguage) {
+      localStorage.setItem("language", newLanguage);
+      this.locale = newLanguage;
+    }
+  },
+  // watch: {
+  //   locale: {
+  //     immediate: true,
+  //     handler(newValue, oldValue) {
+  //       console.log("languaged changed to ", newValue);
+  //     }
+  //   }
+  // }
+};
+</script>
