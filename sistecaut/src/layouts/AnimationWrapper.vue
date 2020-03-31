df6bf8ed4c094bb48e945ad598da30ba8f521547
Andre Amorim <andreamorimsimoes@gmail.com>
2020-03-31 01:03:00 +0100

    add Wrappers (animation / loading)

--- sistecaut/src/layouts/AnimationWrapper.vue
+++ sistecaut/src/layouts/AnimationWrapper.vue
@@ -1,23 +1,131 @@
 <template>
   <div>
+    <ArrowUp
+      class="slideButtons slideUp"
+      v-on:click="handleUpClick"
+      v-if="currentPage > 0"
+    />
+    <ArrowDown
+      class="slideButtons slideDown"
+      v-on:click="handleDownClick"
+      v-if="currentPage < nPages - 1"
+    />
     <slot />
   </div>
 </template>
 
 <script>
+import ArrowUp from "@/assets/svgs/chevrons-up.svg";
+import ArrowDown from "@/assets/svgs/chevrons-down.svg";
 export default {
   name: "AnimationWrapper",
-  props: ["currentPage", "direction"],
-  // watch: {
-  //   currentPage: {
-  //     immediate: true,
-  //     deep: true,
-  //     handler(newValue, oldValue) {
-  //       console.log("new = ", newValue)
-  //     }
-  //   }
-  // },
+  components: {
+    ArrowUp,
+    ArrowDown
+  },
+  props: ["idStart", "nPages", "currentPage", "setCurrentPage"],
+  data() {
+    return {
+      nextPage: null,
+      scrolling: false
+    };
+  },
+  methods: {
+    handleUpClick: function() {
+      this.scrolling = true;
+      if (this.currentPage > 0) {
+        this.nextPage = this.currentPage - 1;
+      } else {
+        this.nextPage = 0;
+      }
+    },
+    handleDownClick: function() {
+      this.scrolling = true;
+      if (this.currentPage < this.nPages - 1) {
+        this.nextPage = this.currentPage + 1;
+      } else {
+        this.nextPage = this.nPages - 1;
+      }
+    },
+    handlePageChange: function() {
+      this.scrolling = false;
+      this.setCurrentPage(this.nextPage);
+      this.nextPage = null;
+    }
+  },
+  // watch the change of the scolling prop
+  watch: {
+    scrolling: {
+      immediate: true,
+      handler(newValue) {
+        if (newValue) {
+          // get the "old" component
+          const oldComp = document.getElementById(
+            `${this.idStart}_${this.currentPage}`
+          );
+
+          // if the old Component exists
+          if (oldComp) {
+            // the old component should execute a fade out with lower scale
+            oldComp.style.opacity = 0;
+            oldComp.style.transform = "scale(0.75)";
+
+            // give the necessary time to the old component to do its animation
+            setTimeout(() => {
+              // go up or down by setting the proper value to the current page
+              this.handlePageChange();
+            }, 500);
+          }
+        }
+      }
+    },
+    currentPage: {
+      immediate: true,
+      handler(newValue, oldValue) {
+        // gives a little of time to the parent component to render the new component
+        setTimeout(() => {
+          // get the new Component
+          const newComp = document.getElementById(
+            `${this.idStart}_${newValue}`
+          );
+          // the new component should execute a fade in to the proper scale animation
+          newComp.style.opacity = "1";
+          newComp.style.transform = "scale(1)";
+        }, 1);
+      }
+    }
   }
+};
 </script>
 
-<style></style>
+<style>
+.slideButtons {
+  z-index: 100;
+  position: absolute;
+  color: white;
+  background-color: rgba(0, 0, 0, .3);
+  height: 5vh;
+  width: 5vh;
+  left: 5px;
+  border-radius: 10vh;
+  border: none;
+  font-weight: lighter;
+  font-size: larger;
+}
+
+.slideButtons:focus {
+  outline: none;
+}
+
+.slideButtons:hover {
+  background-color: var(--grey-color);
+}
+
+.slideUp {
+  top: 15vh;
+}
+
+.slideDown {
+  bottom: 5vh;
+}
+</style>
---
+++ sistecaut/src/layouts/LoadingWrapper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <g-image
+      alt="STAlogo"
+      src="~/assets/icons/staFull.png"
+      width="75px"
+      height="75px"
+      fit="outside"
+      class="logoLoading"
+    /> -->
+    <div class="loader" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingWrapper"
+};
+</script>
+
+<style>
+.loadingPage {
+  display: inline-block;
+  filter: blur(50px) grayscale(.5);
+  pointer-events: none;
+}
+
+.logoLoading {
+  position: absolute;
+  top: calc(50% - 120px - 35px);
+  left: calc(50% - 120px);
+}
+
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #f08013;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: calc(50% - 50px);
+  left: calc(50% - 50px);
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
