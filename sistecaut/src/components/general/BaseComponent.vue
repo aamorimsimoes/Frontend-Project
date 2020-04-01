8406a39fd77558737a36c9987bb8464698cd8578
Andre Amorim <andreamorimsimoes@gmail.com>
2020-04-01 18:23:54 +0100

    create base component for animation and dimension

---
+++ sistecaut/src/components/general/BaseComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="generalDimensions">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "BaseComponent",
+  methods: {
+    handleZoomOut: function() {
+      // the old component should execute a fade out with lower scale
+      if (this.$el) {
+        this.$el.style.opacity = 0;
+        this.$el.style.transform = "scale(0.75)";
+      }
+    },
+    handleZoomIn: function() {
+      // the new component should execute a fade in to the proper scale animation
+      if (this.$el) {
+        this.$el.style.opacity = "1";
+        this.$el.style.transform = "scale(1)";
+      }
+    }
+  }
+};
+</script>
+<style></style>
