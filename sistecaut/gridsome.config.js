1774da258f689273fe88e56f89fb00cc73ca9301
Andre Amorim <andreamorimsimoes@gmail.com>
2020-04-01 17:54:54 +0100

    set rules to scss loader

--- sistecaut/gridsome.config.js
+++ sistecaut/gridsome.config.js
@@ -26,5 +26,21 @@ module.exports = {
       sizes: [76, 152, 120, 167],
       precomposed: true
     }
+  },
+  module: {
+    rules: [
+      // ... other rules omitted
+
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   }
 }
