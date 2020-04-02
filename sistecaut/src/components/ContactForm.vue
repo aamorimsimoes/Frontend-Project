587ed6a2060c0f5a1495b8c4b4f7e7c78f9ed70b
Andre Amorim <andreamorimsimoes@gmail.com>
2020-04-02 02:56:46 +0100

    contactForm Development

--- sistecaut/src/components/ContactForm.vue
+++ sistecaut/src/components/ContactForm.vue
@@ -1,62 +1,80 @@
 <template>
-  <div class="formContact">
-    <form
-      name="contact"
-      method="post"
-      v-on:submit.prevent="handleSubmit"
-      action="/success/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <p hidden>
-        <label> Don’t fill this out: <input name="bot-field" /> </label>
-      </p>
-      <div class="sender-info">
-        <div>
-          <label for="name" class="label">Full name</label>
-          <input
-            placeholder="Write your full name"
-            type="text"
-            name="name"
-            v-model="formData.name"
-            required
-          />
+  <div class="contactWrapper">
+    <div class="formContact">
+      <form
+        name="contact"
+        method="post"
+        v-on:submit.prevent="handleSubmit"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <h3>Formulário de Contato</h3>
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label> Don’t fill this out: <input name="bot-field" /> </label>
+        </p>
+        <div class="sender-info">
+          <div>
+            <label for="name" class="label">Full name</label>
+            <input
+              placeholder="Write your full name"
+              type="text"
+              name="name"
+              v-model="formData.name"
+              required
+            />
+          </div>
+          <div>
+            <label for="email">Email</label>
+            <input
+              placeholder="Write your email"
+              type="email"
+              name="email"
+              v-model="formData.email"
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label for="email">Email</label>
-          <input
-            placeholder="Write your email"
-            type="email"
-            name="email"
-            v-model="formData.email"
-            required
-          />
+
+        <div class="message-wrapper">
+          <label for="message">Message</label>
+          <textarea
+            rows="10"
+            placeholder="Write your message here"
+            name="message"
+            v-model="formData.message"
+          ></textarea>
         </div>
-      </div>
 
-      <div class="message-wrapper">
-        <label for="message">Message</label>
-        <textarea
-          rows="10"
-          placeholder="Write your message here"
-          name="message"
-          v-model="formData.message"
-        ></textarea>
-      </div>
-
-      <button class="buttonSubmit" type="submit">Submit form</button>
-    </form>
+        <button class="buttonSubmit" type="submit">Submit form</button>
+      </form>
+    </div>
+    <div class="footer">
+      <!-- search this information according to calendar date (always updated) -->
+      <span class="footer__copyright"
+        >Copyright © {{ new Date().getFullYear() }}.
+      </span>
+      <span class="footer__links"
+        >Powered by <a href="//gridsome.org"> Gridsome <Vue class="vueSVG"/></a></span
+      >
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "../assets/svgs/vue.svg"
+
 export default {
   data() {
     return {
       formData: {}
     };
   },
+  components: {
+    Vue
+  },
+  name: "ContactForm",
   methods: {
     encode(data) {
       return Object.keys(data)
@@ -78,7 +96,6 @@ export default {
         .catch(error => alert(error));
     }
   },
-  name: "ContactForm"
 };
 </script>
 
@@ -145,5 +162,20 @@ form {
   background: var(--orange-color);
   border-color: var(--orange-color) !important;
   transition: all .8s ease 0 s;
+}
+
+.footer {
+  position: fixed;
+  left: 0;
+  top: 85vh;
+  width: 100%;
+  color: var(--gray-color);
+  text-align: center;
+}
+
+.vueSVG {
+  position: absolute;
+
+  top: -17px;
 }
 </style>
